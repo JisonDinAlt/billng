df5a7935f6a0289375dbf965ae15307e7d8c2dcd
JisonDinAlt <emercom@grodno.net>
2018-11-02 15:08:15 +0300

    rails code style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-has_many :payments 
-has_secure_password
-def payer?
-self.role == 'payer'
-end
-def admin?
-self.role == 'admin'
-end
+  has_many :payments  
+  has_secure_password
+  
+  def payer?
+    self.role == 'payer'
+  end
+  
+  def admin?
+    self.role == 'admin'
+  end
 end
