18c2d8e0334ff23a7fdf9a7b7f6484107ad648c0
JisonDinAlt <emercom@grodno.net>
2018-11-02 14:58:27 +0300

    rails code style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user 
 
-helper_method :current_user 
+  def current_user 
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+  end
 
-def current_user 
-  @current_user ||= User.find(session[:user_id]) if session[:user_id] 
-end
+  def require_user 
+    redirect_to '/login' unless current_user 
+  end
 
-def require_user 
-  redirect_to '/login' unless current_user 
-end
+  def require_payer
+    redirect_to '/no_access' unless current_user.payer?
+  end
 
-def require_payer
-redirect_to '/no_access' unless current_user.payer?
-end
-
-def require_admin
-redirect_to '/no_access' unless current_user.admin?
-end
-
+  def require_admin
+    redirect_to '/no_access' unless current_user.admin?
+  end
 end
