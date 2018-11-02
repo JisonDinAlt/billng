1a52688b51f43f8dbee72aa475680ccbf5d32f3b
JisonDinAlt <emercom@grodno.net>
2018-11-02 15:05:52 +0300

    rails code style

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,39 +1,45 @@
 class UserController < ApplicationController
+  before_action :require_user, only: [:show]
+  before_action :require_payer, only: [:show] 
 
-before_action :require_user, only: [:show]
-before_action :require_payer, only: [:show] 
-def new
-@user = User.new
-end
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @payments = @user.payments
+  end
+  
+  def new
+    @user = User.new
+  end
 
-def create 
-  @user = User.new(user_params) 
-  if @user.save 
-    session[:user_id] = @user.id 
-    redirect_to '/index' 
-  else 
-    redirect_to '/signup' 
-  end 
-end
+  def create 
+    @user = User.new(user_params) 
+    if @user.save 
+      session[:user_id] = @user.id 
+      redirect_to '/index' 
+    else 
+      redirect_to '/signup' 
+    end 
+  end
+  
+  def edit
+  end
 
-def index
-@users = User.all
-end
+  def update
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.delete
+    redirect_to '/index'
+  end
 
-def show
-@user = User.find(params[:id])
-@payments = @user.payments
-end
-
-def destroy
-@user = User.find(params[:id])
-@user.delete
-redirect_to '/index'
-end
-
-private 
-def user_params 
-params.require(:user).permit(:first_name, :last_name, :email, :password)
-end 
-
+  private 
+  
+    def user_params 
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end 
 end
